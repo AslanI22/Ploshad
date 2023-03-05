669e4b8c1cfc9a412cdff5acf63b592fa6c15d4b
AslanI22 <reshetov.aslan.i22@gmail.com>
2023-03-05 23:20:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            uwu program = new uwu();
+            program.Run();
+        }
+    }
+}
---
+++ uwu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Banan
+{
+    internal class uwu
+    {
+        private double pi = 3.14;
+        public void Run()
+        {
+            
+            Console.Write("Введите радиус окружности: ");
+            double R = Convert.ToInt32(Console.ReadLine());
+            double S = pi * R * R;
+            Console.WriteLine("Результат: S = " + (S));
+        }
+    }
+}
